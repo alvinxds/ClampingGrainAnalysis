24b887106513283a6a4c0bb5506ea1ee94654b39
nkroell <nils.kroell@iar.rwth-aachen.de>
2019-11-30 01:10:08 +0100

    Improved masking and added matching. Fixes #3.

--- clampingGrain.m
+++ clampingGrain.m
@@ -9,7 +9,7 @@ addpath(genpath('.\functions'));
 
 
 % Settings
-STARTING_FOLDER = 'C:\Users\Nils Kröll\sciebo\IAR\Klemmkornauswertung\Testbilder Programmentwicklung\Bilder Testsiebung 20191118-1605';
+STARTING_FOLDER = 'C:\Users\Nils Kröll\sciebo\IAR\Klemmkorn Paper\020 Messreihen\001 SSF fein\Bilder Siebboden\clean';
 COVERAGE_CLASS_EDGES = [0,eps,0.1,0.9,1];
 
 % Settings Marker
@@ -29,14 +29,12 @@ fprintf('Loading images ... ')
 [img_material, path_material_img, filename_material_img] = readImgFromUI('Select image of sieving surface with clamping grain.', updated_starting_folder);
 tic
 % the results are saved in the material img folder:
-path_xlsx = path_material_img;
+path_results = path_material_img;
 clear path_material_img
 
 % the stats are saved in a file that is named after the filename of the material image:
-filename_xlsx = getXLSXFilename(filename_material_img);
+filename_material_img_withoutending = removeFileEndingFromFilename(filename_material_img);
 
-% full filename is then given by:
-fullfilename_xlsx = fullfile(path_xlsx, filename_xlsx);
 fprintf(' done!\n')
 
 %% MARKER DETECTION
@@ -65,8 +63,8 @@ marker_stats_material = getMarkerStatsLAB(img_material_rotated, marker_mean_colo
 % crop images
 img_clean_cropped = cropImageBasedOnMarkerPositions(img_clean_rotated, marker_stats_clean, MARKER_MARGIN_CROP);
 img_material_cropped = cropImageBasedOnMarkerPositions(img_material_rotated, marker_stats_material, MARKER_MARGIN_CROP);
-figure
-imshowpair(img_clean_cropped, img_material_cropped, 'montage')
+% figure
+% imshowpair(img_clean_cropped, img_material_cropped, 'montage')
 
 % update marker positions and stats for rotated and cropped image
 marker_stats_clean = getMarkerStatsLAB(img_clean_cropped, marker_mean_colorvalue_lab, threshold_lab_distance, N_MARKER);
@@ -75,9 +73,8 @@ marker_stats_material = getMarkerStatsLAB(img_material_cropped, marker_mean_colo
 % show found markers
 img_clean_with_marker = drawMarkerPositionsOnImg(img_clean_cropped, marker_stats_clean);
 img_material_with_marker = drawMarkerPositionsOnImg(img_material_cropped, marker_stats_material);
-figure
-imshowpair(img_clean_with_marker, img_material_with_marker, 'montage')
-fprintf(' done!\n')
+% figure
+% imshowpair(img_clean_with_marker, img_material_with_marker, 'montage')
 
 % calculate the global calibration factor based on the known radius of the
 % marker
@@ -104,45 +101,69 @@ fprintf('Segmentation of both images ... ')
 bw_clean = imbinarize(rgb2gray(img_clean_cropped));
 bw_material = imbinarize(rgb2gray(img_material_cropped_warped));
 
-figure
-imshowpair(bw_clean, bw_material, 'montage');
+% figure
+% imshowpair(bw_clean, bw_material, 'montage');
 
 fprintf(' done!\n')
-
-%% MATCHING
-% fprintf('Matching of detected sieving holes ... ')
-% stats_to_be_calculated = {'Centroid', 'Area'};
-% stats_clean = regionprops(bw_clean, stats_to_be_calculated);
-% stats_material = regionprops(bw_material, stats_to_be_calculated);
-% 
-% [stats_clean,stats_material] = addNearestRegionToStats(stats_clean,stats_material);
-% [stats_clean,stats_material] = addAreasOfCorrespondingRegionToStats(stats_clean,stats_material);
-% 
-% fprintf(' done!\n')
 
 %% OVERLAY MASKS
 fprintf('Overlaying masks ... ')
 overlay_image = overlayMasks(bw_clean, bw_material);
 
-figure
-imshow(overlay_image)
+% figure
+% imtool(overlay_image)
 
 fprintf(' done!\n')
 %% ANALYSIS
-% fprintf('Analysis of detected clamping grain ... ')
-% % object stats (sieving holes)
-% object_stats = calcObjectStats(stats_clean, global_calibfactor);
-% 
-% % counting of sieving holes with specific coverage
-% counting_stats = getCountingStats(object_stats, COVERAGE_CLASS_EDGES);
-% 
-% % overall stats
-% overall_stats = calcOverallStats(object_stats);
-% 
-% 
-% % export stats as .xlsx
-% saveResultsAsXLSX(object_stats, overall_stats, counting_stats, fullfilename_xlsx)
-% fprintf(' done!\n')
-% fprintf('Results:\n')
-% dispLinkToFolder(fullfilename_xlsx)
-% toc
+fprintf('Analysis of detected clamping grain ... ')
+
+bw_overlay = overlay_image > 0;
+object_stats = regionprops(bw_overlay, overlay_image, {'Area', 'PixelValues', 'BoundingBox', 'Centroid'});
+
+% object stats (sieving holes)
+object_stats = calcObjectStats(object_stats, global_calibfactor);
+
+% counting of sieving holes with specific coverage
+counting_stats = getCountingStats(object_stats, COVERAGE_CLASS_EDGES);
+
+% overall stats
+overall_stats = calcOverallStats(object_stats, global_calibfactor);
+
+fprintf(' done!\n')
+
+%% SAVE RESULTS
+fprintf('Saving results ... ')
+results_folder_name = ['results_', filename_material_img_withoutending];
+path_results_subfolder = fullfile(path_results, results_folder_name);
+
+mkdir(path_results_subfolder);
+
+% create new subfolder
+% save stats as .xlsx
+filename_xlsx = ['stats_', filename_material_img_withoutending, '.xlsx'];
+saveResultsAsXLSX(object_stats, overall_stats, counting_stats, fullfile(path_results_subfolder, filename_xlsx));
+
+% save images
+% overlay image
+filename_overlay_img = ['overlay_', filename_material_img_withoutending, '.png'];
+imwrite(overlay_image, fullfile(path_results_subfolder, filename_overlay_img));
+
+%  cropped and rotate clean image
+filename_img_clean = ['img_clean_', filename_material_img_withoutending, '.png'];
+imwrite(img_clean_cropped, fullfile(path_results_subfolder, filename_img_clean));
+
+filename_img_clampinggrain = ['img_clampinggrain_', filename_material_img_withoutending, '.png'];
+imwrite(img_material_cropped_warped, fullfile(path_results_subfolder, filename_img_clampinggrain));
+
+% binary images
+filename_bw_clean = ['bw_clean_', filename_material_img_withoutending, '.png'];
+imwrite(bw_clean, fullfile(path_results_subfolder, filename_bw_clean));
+
+filename_bw_clampinggrain = ['bw_clampinggrain_', filename_material_img_withoutending, '.png'];
+imwrite(bw_material, fullfile(path_results_subfolder, filename_bw_clampinggrain));
+
+
+fprintf(' done!\n')
+fprintf('Results:\n')
+dispLinkToFolder(path_results_subfolder)
+toc
